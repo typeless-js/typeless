491cdb66e83b5c9ddb77ea4a3a6a4bef20fe2ece
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2019-02-20 10:25:19 +0100

    switch to monorepo
    Signed-off-by: Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>

--- packages/typeless/scripts/build.sh
+++ packages/typeless/scripts/build.sh
@@ -3,5 +3,5 @@ mkdir dist
 yarn run tsc --declaration true --module esnext --outDir "./dist/es"
 yarn run tsc --declaration true --module commonjs --outDir "./dist"
 cp package.json ./dist/package.json
-cp README.md ./dist/README.md
+cp ../../README.md ./dist/README.md
 rm -rf ./dist/__tests__
