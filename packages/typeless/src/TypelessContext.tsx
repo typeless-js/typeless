7dbcad03f889c7507bddd01dd632a3966b00ae15
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2019-06-07 16:15:37 +0200

    add default registry

--- packages/typeless/src/TypelessContext.tsx
+++ packages/typeless/src/TypelessContext.tsx
@@ -5,12 +5,14 @@ export const TypelessContext = React.createContext(null as null | {
   registry: Registry;
 });
 
+export const defaultRegistry = new Registry();
+
 export function DefaultTypelessProvider({
   children,
 }: {
   children: React.ReactChild;
 }) {
-  const value = React.useMemo(() => ({ registry: new Registry() }), []);
+  const value = React.useMemo(() => ({ registry: defaultRegistry }), []);
   return (
     <TypelessContext.Provider value={value}>
       {children}
