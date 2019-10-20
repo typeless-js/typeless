41928ef168a1e678f5d7b1957f4d647aa5bbb2fa
sisisin <azsisisin@gmail.com>
2019-10-20 17:16:12 +0900

    fix: allow to enable module in multiple Registry instances

--- packages/typeless/__tests__/createModule.test.tsx
+++ packages/typeless/__tests__/createModule.test.tsx
@@ -1,4 +1,9 @@
+import React, { createElement } from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
 import { createModule } from '../src/createModule';
+import { Registry } from '../src/Registry';
+import { TypelessContext } from '../src/TypelessContext';
 
 test('createModule with actions', () => {
   const [, Actions] = createModule(Symbol('foo')).withActions({
@@ -49,4 +54,49 @@ test('createModule with actions and state', () => {
     }
   `);
   expect(getState()).toBe(undefined);
+});
+
+describe('use module at multiple registries', () => {
+  let container: HTMLDivElement = null!;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null!;
+  });
+
+  function render(registry: Registry, factory: () => JSX.Element) {
+    act(() => {
+      ReactDOM.render(
+        <TypelessContext.Provider value={{ registry }}>
+          {createElement(factory)}
+        </TypelessContext.Provider>,
+        container
+      );
+    });
+  }
+
+  it('should be initialized', () => {
+    const [handle] = createModule(Symbol('foo')).withState<number>();
+    handle.reducer(1);
+
+    const registry1 = new Registry();
+    render(registry1, () => {
+      handle();
+      return null;
+    });
+
+    const registry2 = new Registry();
+    render(registry2, () => {
+      handle();
+      return null;
+    });
+
+    expect(registry1.getState()).toStrictEqual({ foo: 1 });
+    expect(registry2.getState()).toStrictEqual({ foo: 1 });
+  });
 });
