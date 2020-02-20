885a2e00c8d5d184c75f2e9b5e11f0f092a62a8a
sisisin <azsisisin@gmail.com>
2020-02-20 18:26:00 +0900

    refactor: move stream creation into epic (#46)
    * refactor: move creating stream to epic
    
    * refactor: Epic's handlers should be private because unused from out of class

--- packages/typeless/src/createOutputStream.ts
+++ packages/typeless/src/createOutputStream.ts
@@ -1,46 +1,7 @@
-import {
-  Subject,
-  queueScheduler,
-  merge,
-  empty,
-  defer,
-  from,
-  of,
-  Observable,
-} from 'rxjs';
-import { mergeMap, observeOn, subscribeOn, catchError } from 'rxjs/operators';
-import { Deps, Action } from './types';
+import { empty, merge, Observable, queueScheduler, Subject } from 'rxjs';
+import { mergeMap, observeOn, subscribeOn } from 'rxjs/operators';
 import { Store } from './Store';
-import { logAction, isAction } from './utils';
-
-function getHandlers(stores: Store[], action: Action) {
-  const [symbol, type] = action.type;
-  return stores
-    .filter(store => {
-      if (!store.isEnabled || !store.epic) {
-        return false;
-      }
-      const { handlers, moduleHandlers } = store.epic;
-
-      return (
-        moduleHandlers.has(symbol) ||
-        (handlers.has(symbol) && handlers.get(symbol)!.has(type))
-      );
-    })
-    .map(store => {
-      const { handlers, moduleHandlers } = store.epic!;
-      return [
-        ...(handlers.has(symbol) && handlers.get(symbol)!.has(type)
-          ? handlers.get(symbol)!.get(type)
-          : []),
-        ...(moduleHandlers.has(symbol) ? moduleHandlers.get(symbol) : []),
-      ].map(handler => ({ store, handler }));
-    })
-    .reduce((ret, arr) => {
-      ret.push(...arr);
-      return ret;
-    }, []);
-}
+import { Action, Deps } from './types';
 
 export function createOutputStream(
   action$: Subject<Action>,
@@ -51,66 +12,16 @@ export function createOutputStream(
     subscribeOn(queueScheduler),
     observeOn(queueScheduler),
     mergeMap(sourceAction => {
-      const handlers = getHandlers(stores, sourceAction);
-      if (!handlers.length) {
+      const handlers = stores
+        .map(store => store.getOutputStream(sourceAction, deps))
+        .filter(handler => handler !== null)
+        .reduce((ret, arr) => [...ret, ...arr], [])!;
+
+      if (handlers.length === 0) {
         return empty();
+      } else {
+        return merge(...handlers);
       }
-      return merge(
-        ...handlers.map(({ store, handler }) => {
-          return defer(() => {
-            const name = store.displayName;
-            if (process.env.NODE_ENV === 'development') {
-              logAction(name, sourceAction);
-            }
-            const result = handler(
-              sourceAction.payload,
-              deps,
-              sourceAction
-            ) as (Observable<Action> | Action | Action[]);
-            if (Array.isArray(result)) {
-              return from(result);
-            }
-            if (isAction(result)) {
-              return of(result);
-            }
-            return result;
-          }).pipe(
-            mergeMap((action: Action) => {
-              if (action === null) {
-                // ignore if action is null
-                return empty();
-              }
-              if (action === undefined) {
-                console.error('Undefined action returned in epic.', {
-                  action,
-                  store: name,
-                });
-                return empty();
-              }
-              if (!isAction(action)) {
-                console.error('Invalid action returned in epic.', {
-                  sourceAction,
-                  action,
-                  store: name,
-                });
-                return empty();
-              }
-              return of(action);
-            }),
-            catchError(err => {
-              console.error(
-                'An unhandled error occurred in epic.',
-                {
-                  sourceAction,
-                  store: name,
-                },
-                err
-              );
-              return empty();
-            })
-          );
-        })
-      );
     })
   );
 }
