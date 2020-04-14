19d680616401fde2c597f6271ff368094f221a21
t7yang <t7yang@gmail.com>
2020-04-14 22:02:08 +0800

    Refactor tsconfig (#60)
    * build(typescript): add share tsconfig and extends in each packages
    
    * build(scripts): update prepare script and remove previous build sh file
    
    * style: fix tsconfig.x.json format

--- packages/typeless/scripts/build.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p dist
-rm -rf ./dist/*
-yarn run tsc --declaration true --declarationMap true --module esnext --outDir "./dist/es"
-yarn run tsc --declaration true --declarationMap true --module commonjs --outDir "./dist"
