c9ba041093e02be11ab3351039c13d5d02ed15fd
sisisin <azsisisin@gmail.com>
2020-08-08 16:45:13 +0900

    test: cover test case of actions with different signatures

--- packages/typeless/__tests__/type/Epic.test.ts
+++ packages/typeless/__tests__/type/Epic.test.ts
@@ -6,11 +6,11 @@ function createAction<T extends ActionMap>(acts: T) {
   return createModule(Symbol('test')).withActions(acts)[1];
 }
 TT.describe('onMany', () => {
-  const m1 = createAction({ foo: () => ({ payload: 1 }) });
-  const m2 = createAction({ foo: () => ({ payload: true }) });
+  const m1 = createAction({ foo: (num: number) => ({ payload: { num } }) });
+  const m2 = createAction({ foo: (boo: boolean) => ({ payload: { boo } }) });
   const m3 = createAction({ foo: () => ({ payload: 'bar' }) });
   const m4 = createAction({ foo: () => ({ payload: Symbol('baz') }) });
-  const m5 = createAction({ foo: () => ({ payload: { obj: 'foo' } }) });
+  const m5 = createAction({ foo: (str: string) => ({ payload: { str } }) });
   const m6 = createAction({ foo: () => ({ payload: ['array'] }) });
 
   TT.describe('should error called with', () => {
@@ -27,11 +27,11 @@ TT.describe('onMany', () => {
     TT.it('called with many arguments', () => {
       new Epic().onMany([m1.foo, m2.foo, m3.foo, m4.foo, m5.foo, m6.foo], p => {
         type Expected =
-          | number
-          | boolean
+          | { num: number }
+          | { boo: boolean }
           | string
           | symbol
-          | { obj: string }
+          | { str: string }
           | string[];
         TT.assert<TT.Eq<typeof p, Expected>>();
 
